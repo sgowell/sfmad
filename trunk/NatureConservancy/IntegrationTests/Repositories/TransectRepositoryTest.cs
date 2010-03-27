c9342edb7848333a79c3a01137555f4ccff42560
ignu.smith <ignu.smith@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-27 09:47:09 +0000

    added libs

---
+++ trunk/NatureConservancy/IntegrationTests/Repositories/TransectRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentNHibernate.Testing;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace IntegrationTests.Repositories
+{
+    [TestFixture]
+    public class CountyRepositorySpecs
+    {
+        protected ISession GetSession()
+        {
+            return new SessionFactoryFactory().GetSessionFactory().OpenSession();
+        }
+
+        [SetUp]
+        public void CreateSchema()
+        {
+
+            var factory = new SessionFactoryFactory();
+            factory.OnFactoryCreation = configuration =>
+            {
+                var exporter = new SchemaExport(configuration);
+                exporter.Create(true, true);
+                exporter.Execute(false, true, false, true);
+                Console.WriteLine("done");
+            };
+            var sessionFactory = factory.GetSessionFactory();
+        }
+        [Test]
+        public void can_persist()
+        {
+            new PersistenceSpecification<County>(GetSession())
+                .CheckProperty(x => x.Name, "Luzene")
+                .VerifyTheMappings();
+        }
+
+
+
+
+    }
+}
