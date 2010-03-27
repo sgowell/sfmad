db95021f29ed0f2a2f4fd9d7ba3106bf82b9902e
scott.gowell <scott.gowell@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-27 04:49:16 +0000

    New Project Layout

---
+++ NatureConservancy/Web/Models/UnderstorySection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NatureConservatory
+{
+    public class UnderStorySection
+    {
+        public Survey Survey
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public IList<UnderStorySpecies> UnderstorySpecies
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Note
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
