90682d5334f568ccfb26bd5ffeb086cc4541329e
swiggin1@bigevolution.com <swiggin1@bigevolution.com@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-27 15:49:50 +0000

    Fill out CsvWriter

--- NatureConservancy/UnitTests/CsvWriterTests.cs
+++ NatureConservancy/UnitTests/CsvWriterTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using IntegrationTests;
+﻿using IntegrationTests;
 using NUnit.Framework;
 using Web.Services;
 
@@ -12,14 +8,49 @@ namespace UnitTests
     public class CsvWriterTests
     {
         [Test]
-        public void creates_csv_columns()
+        public void creates_csv_columns_with_break()
         {
             var writer = new StringCsvWriter();
+            writer.Delimiter = '|';
+            writer.AddCell("hello");
+            writer.AddCell("world");
+            writer.EndLine();
+            writer.Writer.ToString().ShouldEqual("hello|world\r\n");
+        }
+
+        [Test]
+        public void creates_csv_columns_with_comma()
+        {
+            var writer = new StringCsvWriter();
+            writer.Delimiter = ',';
             writer.AddCell("hello");
             writer.AddCell("world");
             writer.EndLine();
             writer.Writer.ToString().ShouldEqual("hello,world\r\n");
-            Assert.Ignore("Consider Delimiters");
+        }
+
+        [Test]
+        public void creates_csv_columns_with_comma_and_quotes()
+        {
+            var writer = new StringCsvWriter();
+            writer.Delimiter = ',';
+            writer.UseQuotes = true;
+            writer.AddCell("hello");
+            writer.AddCell("world");
+            writer.EndLine();
+            writer.Writer.ToString().ShouldEqual("\"hello\",\"world\"\r\n");
+        }
+
+        [Test]
+        public void creates_csv_columns_with_comma_and_escaped_quotes()
+        {
+            var writer = new StringCsvWriter();
+            writer.Delimiter = ',';
+            writer.UseQuotes = true;
+            writer.AddCell("he said \"the dude abides\"");
+            writer.AddCell("world");
+            writer.EndLine();
+            writer.Writer.ToString().ShouldEqual("\"he said \"\"the dude abides\"\"\",\"world\"\r\n");
         }
     }
 }
