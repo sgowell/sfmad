50c5cb87f09b51086f6b0fc885d29e6bf9904ec1
ignu.smith <ignu.smith@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-27 12:55:31 +0000

    started mapping stands

--- trunk/NatureConservancy/Web/Models/Mappings/TransectMapping.cs
+++ trunk/NatureConservancy/Web/Models/Mappings/TransectMapping.cs
@@ -2,15 +2,24 @@
 
 namespace Web.Models.Mappings
 {
+    public class StandMapping : ClassMap<Stand>
+    {
+        public StandMapping()
+        {
+            Id(x => x.Id);
+            Map(x => x.Number);
+        }
+    }
     public class TransectMapping : ClassMap<Transect>
     {
         public TransectMapping()
         {
             Id(x => x.Id);
-            Map(x => x.TransectNumber);
+            Map(x => x.Number);
             Map(x => x.TransectClass);
             Map(x => x.Longitude);
             Map(x => x.Latitude);
+            //Map(x => x.Stands, new Stand());
         }
     }
 }
