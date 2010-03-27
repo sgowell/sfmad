19a548ccd9a2fc77792a1fcc9b914175d1f2b5a7
scott.gowell <scott.gowell@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-27 18:07:02 +0000

    Removing Unnecessary Models. Adding Validation to Models: Overstory OverstoryItem Plot PlotItem Survey

--- NatureConservancy/Web/Models/Overstory.cs
+++ NatureConservancy/Web/Models/Overstory.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Web.Models
 {
-    public class OverStory
+    public class Overstory
     {
         [DisplayName("Notes")]
+        [UIHint("TextArea")]
         public String Notes{ get; set;}
-        public IList<OverstoryItem> OverStoryItems { get; set; }
+
+        public IList<OverstoryItem> OverstoryItems { get; set; }
+
         public IList<Snag> Snags{get;set;}
         public Microtopography Microtopography { get; set; }
     }
