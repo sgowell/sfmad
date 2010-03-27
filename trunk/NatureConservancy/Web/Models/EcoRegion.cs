664fd892987b9cb87730503ae221fe8d4d36578d
scott.gowell <scott.gowell@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-27 16:43:54 +0000

    Adding Validation to EcoRegion Model.

--- trunk/NatureConservancy/Web/Models/EcoRegion.cs
+++ trunk/NatureConservancy/Web/Models/EcoRegion.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Models
 {
     public class EcoRegion
     {
+        [Required(ErrorMessage = "Please enter a {0}")]
+        [DisplayName("Region Number")]
         public int RegionNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter a {0}")]
+        [DisplayName("Region Name")]
         public string Name { get; set; }
+
         public IList<Site> Sites { get; set; }
 
     }
