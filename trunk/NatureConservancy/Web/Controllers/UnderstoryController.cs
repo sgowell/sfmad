15f6eb3f5779314ddc339ebec1e63a95908157fe
scott.gowell <scott.gowell@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 17:52:22 +0000

    WoodyDebrisControllerTests: Killed Unnecessary References
    ControllerBase:
    Added New Understory and UnderstoryItem methods
    
    /Web/Controllers/
    Added Understory and Understory Item respective Controllers
    
    Updated Survey:
    Added AddUnderstory
    
    Removed Mappings folder from project.

---
+++ trunk/NatureConservancy/Web/Controllers/UnderstoryController.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Web.Mvc;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class UnderstoryController : ControllerBase
+    {
+        protected IRepository<Understory> UnderstoryRepository;
+        public UnderstoryController(ISurveyRepository surveyRepository, IRepository<Understory> UnderstoryRepository)
+            : base(surveyRepository)
+        {
+            this.UnderstoryRepository = UnderstoryRepository;
+        }
+
+        public ActionResult New(int surveyId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(int surveyId, Understory understory)
+        {
+            UnderstoryRepository.Save(understory);
+            var survey = surveyRepository.Load(surveyId);
+            survey.AddUnderstory(understory);
+            UnderstoryRepository.Save(understory);
+            return NewUnderstory(survey);
+        }
+
+    }
+}
