5cd2e2babe9eeac4ea07cc17040c52e95b151b1f
ignu.smith <ignu.smith@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 17:57:56 +0000

    better js and new site c action

--- trunk/NatureConservancy/Web/Scripts/site.js
+++ trunk/NatureConservancy/Web/Scripts/site.js
@@ -8,36 +8,61 @@ $(function() {
   sf.init();
 });
 
+$.fn.appendSelect = function(selectItems) {
+    if (!isArray(selectItems)) selectItems = [selectItems];
+    var optionHtml = "";
+    $.each(selectItems, function() {
+        optionHtml = optionHtml + '<option value="' + this.Id + '>' + this.Name + '</option>';
+    });
+    $(this).append(optionHtml);
+}
+
+$.fn.reset = function() {
+  $(this)
+    .not(':button, :submit, :reset, :hidden')
+    .val('')
+    .removeAttr('checked')
+    .removeAttr('selected');
+
+}
 $.fn.handleLookup = function() {
 
     var title = $(this).attr("title");
     var name = $(this).attr("name");
     var path = "/" + name + "/new";
-
+    var allPath = "/" + name + "/all";
+    
+    $("." + name + "-dropdown").appendSelect({Name:"Select...", Id:0});
+    
+    $.post(allPath, function(data) {                
+        $("." + name + "-dropdown").appendSelect(data);               
+    });
+    
     var d = $("<div>").attr("id", name).css('display', 'none').appendTo('body');
     $.get(path, function(data) { d.html(data); });
 
     var callback = function(item) {
         $.post(path, d.find('form').serialize(),
-            function(i) {
+            function(data) {
                 d.dialog('close');
-                var selector = "." + name + "-dropdown";
-
-                $(selector).append($("<option></option>").
-                    attr("value", i.Entity.Id).
-                    text(i.Entity.Name));
+                $("." + name + "-dropdown").appendSelect(data.Entity);
+                d.find('form').reset();
             });
-
     };
+
     $(this).click(function() {
         d.dialog({
             modal: true,
             width: 500,
-            buttons: { "Submit": callback }
-        });
+            buttons: { "Submit": callback }});
         $('.ui-dialog-title').text(title);
         d.dialog('open');
+        
         return false;
     });
 
+}
+
+function isArray(obj) {
+  return obj.constructor == Array;
 }
