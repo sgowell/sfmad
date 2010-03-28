34914934e28214d19f0ff0860ddbab9ea14fbaa3
scott.gowell <scott.gowell@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 14:17:33 +0000

    Update Base Controller Added 2 new Controllers. Updating CSProj for new files.

---
+++ NatureConservancy/Web/Controllers/DeerHerbivoryController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class DeerHerbivoryController : ControllerBase
+    {
+        protected IRepository<DeerHerbivory> DeerHerbivoryRepository;
+        public DeerHerbivoryController(ISurveyRepository surveyRepository, IRepository<DeerHerbivory> deerHerbivoryRepository) : base(surveyRepository)
+        {
+            this.DeerHerbivoryRepository = deerHerbivoryRepository;
+        }
+
+        public ActionResult New(int surveyId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(int surveyId, DeerHerbivory deerHerbivory)
+        {
+            DeerHerbivoryRepository.Save(deerHerbivory);
+            var survey = surveyRepository.Load(surveyId);
+            survey.DeerHerbivory.Add(deerHerbivory);
+            return NewDeerHerbivory(survey);
+        }
+    }
+}
