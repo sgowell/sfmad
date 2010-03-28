1262495c8958bf1249dc13fee1cb5d58d569beb4
scott.gowell <scott.gowell@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 14:56:47 +0000

    Adding GroundCoverController Updating ControllerBase for new Controller.

---
+++ NatureConservancy/Web/Controllers/GroundCoverController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class GroundCoverController : ControllerBase
+    {
+        protected IRepository<GroundCover> GroundCoverRepository;
+        public GroundCoverController(ISurveyRepository surveyRepository, IRepository<GroundCover> groundCoverRepo)
+            : base(surveyRepository)
+        {
+            this.GroundCoverRepository = groundCoverRepo;
+        }
+
+        public ActionResult New(int surveyId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(int surveyId, GroundCover cover)
+        {
+            GroundCoverRepository.Save(cover);
+            var survey = surveyRepository.Load(surveyId);
+            survey.GroundCover = cover;
+            return NewGroundCover(survey);
+        }
+    }
+}
