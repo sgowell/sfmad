027d96c6c7df4d224f00e54777a5c92a298203e5
vincent.gillon <vincent.gillon@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 03:46:59 +0000

    Created EcoSystem.cs

---
+++ trunk/NatureConservancy/Web/Models/EcoSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+	public class EcoSystem : Entity
+	{
+
+        public virtual String EcoSystemName
+        {
+            get; set;
+        }
+	}
+}
