813130a54ab46ab4926291b186b1e8b1addc0f1d
scott.gowell <scott.gowell@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 14:57:22 +0000

    [FIX] Adding incomplete Plot and PlotItem Controllers.

---
+++ trunk/NatureConservancy/Web/Controllers/PlotItemController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Data;
+using Web.Models;
+using Web.Services;
+
+namespace Web.Controllers
+{
+    public class PlotItemController : ControllerBase
+    {
+        protected IRepository<PlotItem> PlotItemRepository;
+        public PlotItemController(ISurveyRepository surveyRepository, IRepository<PlotItem> plotItemRepo) : base(surveyRepository)
+        {
+            this.PlotItemRepository = plotItemRepo;
+        }
+
+        public ActionResult New(int surveyId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(int surveyId, PlotItem plotItem)
+        {
+            PlotItemRepository.Save(plotItem);
+            var survey = surveyRepository.Load(surveyId);
+            Plot parent = survey.GroundCover.Plots.Single(p => p.Id == plotItem.Plot.Id);
+            parent.PlotItems.Add(plotItem);
+            
+            return NewPlotItem(survey);
+        }
+
+        //
+        // GET: /PlotItem/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
