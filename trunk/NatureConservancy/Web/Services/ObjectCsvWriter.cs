3de5be84e26beb613c4a93ee82a3901665c42e59
swiggin1@bigevolution.com <swiggin1@bigevolution.com@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 01:10:44 +0000

    Object to CSV writer

---
+++ trunk/NatureConservancy/Web/Services/ObjectCsvWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Web.Services
+{
+    public class ObjectCsvWriter
+    {
+        private readonly ObjectConverter _ObjectConverter = new ObjectConverter();
+
+        public void WriteToCSV(IEnumerable collection, Stream outputStream, bool useQuotes)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            if (outputStream == null)
+                throw new ArgumentNullException("outputStream");
+
+            using (ICsvWriter csvWriter = new StreamCsvWriter(outputStream))
+            {
+                csvWriter.UseQuotes = useQuotes;
+
+                foreach (var item in collection)
+                {
+                    _WriteItem(csvWriter, item);
+                }
+            }
+        }
+
+        private void _WriteItem(ICsvWriter csvWriter, object item)
+        {
+            var dictionary = _ObjectConverter.ToDictionary(item);
+
+            foreach (var dictionaryEntry in dictionary)
+            {
+                string value = (dictionaryEntry.Value == null) ? string.Empty : dictionaryEntry.Value.ToString();
+
+                csvWriter.AddCell(value);
+            }
+
+            csvWriter.EndLine();
+        }
+    }
+}
