d7aa54003f459bbed5ca0958e9c01e8b32292996
intuitionevan <intuitionevan@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-03-28 05:47:18 +0000

    Attribute to require login.

---
+++ NatureConservancy/Web/Controllers/RequiresAuthentication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Web.Controllers
+{
+    public class RequiresAuthentication : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext  actionContext)
+        {
+            if(!actionContext.HttpContext.User.Identity.IsAuthenticated )
+            {
+                string redirectOnSuccess = actionContext.HttpContext.Request.Url.AbsolutePath;
+
+                string redirectUrl = String.Format("?ReturnUrl={0}", redirectOnSuccess);
+                string loginUrl = FormsAuthentication.LoginUrl + redirectUrl;
+                actionContext.HttpContext.Response.Redirect(loginUrl,true);
+
+            }
+        }
+    }
+}
