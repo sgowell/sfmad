5f856a9aa00ae83a0fae656f391ac2716393bd8a
cbroland <cbroland@4ac0118b-764e-af5c-a0fb-853c77d29a42>
2010-05-04 03:51:37 +0000

    Working on woody debris page.

--- trunk/NatureConservancy/Web/Controllers/WoodyDebrisController.cs
+++ trunk/NatureConservancy/Web/Controllers/WoodyDebrisController.cs
@@ -25,8 +25,8 @@ namespace Web.Controllers
         [HttpPost]
         public ActionResult New(int surveyId, WoodyDebris woodyDebris)
         {
-            woodyDebrisRepository.Save(woodyDebris);
-            var survey = surveyRepository.Load(surveyId);            
+            woodyDebrisRepository.Save(woodyDebris); 
+            var survey = surveyRepository.Load(surveyId);                                   
             survey.WoodyDebris.Add(woodyDebris);
             return NewWoodyDebris(survey);
         }
